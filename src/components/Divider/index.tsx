7a191774bfa35f3c50df71802b7ab158dd5134e6
lipliepra <dzis99@yandex.ru>
2024-05-13 20:42:44 +0500

    Обновил описание

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -2,5 +2,5 @@ import { type FC } from 'react';
 
 import './index.scss';
 
-/** Divider - компонент разделитель контента */
+/** Компонент Divider представляет собой простой разделительный элемент. */
 export const Divider: FC = () => <div className='apollo-divider' />;
